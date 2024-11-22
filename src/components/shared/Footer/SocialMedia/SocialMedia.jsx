afac51f905c435430eb79c736979693bff55fb68
MinaNagy99 <minanagykhalefa@gmail.com>
2024-11-22 12:31:38 +0200

    e'

--- src/components/shared/Footer/SocialMedia/SocialMedia.jsx
+++ src/components/shared/Footer/SocialMedia/SocialMedia.jsx
@@ -8,7 +8,7 @@ function SocialMedia() {
         <div className="col-span-1 mx-2  flex justify-center items-center">
           <Link
             target="_blank"
-            to="https://www.instagram.com/mosaicholding_corporation/"
+            to="https://www.instagram.com/wolsey_structuralengineering/"
           >
             <i className="fa-brands fa-instagram text-mainBrown rounded-full p-2 custom-text-lg border-mainBrown border-[3px]"></i>
           </Link>
@@ -16,20 +16,16 @@ function SocialMedia() {
         <div className="col-span-1 mx-2  flex justify-center items-center">
           <Link
             target="_blank"
-            to="https://www.facebook.com/profile.php?id=61565919099335"
+            to="https://www.facebook.com/profile.php?id=61565888981089"
           >
             <i className="fa-brands fa-facebook text-mainBrown rounded-full p-2 custom-text-lg border-mainBrown border-[3px]"></i>
           </Link>
         </div>
-        <div className="col-span-1 mx-2  flex justify-center items-center">
-          <Link target="_blank" to="https://x.com/MosaicHoldingCo">
-            <i className="fa-brands fa-x-twitter text-mainBrown rounded-full p-2 custom-text-lg border-mainBrown border-[3px]"></i>
-          </Link>
-        </div>
+
         <div className="col-span-1 mx-2  flex justify-center items-center">
           <Link
             target="_blank"
-            to="https://www.linkedin.com/company/mosaic-holding-corporation/?viewAsMember=true"
+            to="https://www.linkedin.com/company/wolsey-structural-engineering-ltd-/?originalSubdomain=ca"
           >
             <i className="fa-brands fa-linkedin text-mainBrown rounded-full p-2 custom-text-lg border-mainBrown border-[3px]"></i>
           </Link>
