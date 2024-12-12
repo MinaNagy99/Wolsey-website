e5b0fd9a87b1720c6549302a75e786c1f145f815
MinaNagy99 <minanagykhalefa@gmail.com>
2024-12-12 11:25:08 +0200

    r

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
+import withMT from "@material-tailwind/react/utils/withMT";
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -11,7 +10,7 @@ module.exports = withMT({
         mainGray: "#F3F1EC",
         secondGray: "#EDEBE6",
       },
-      //HELLO MINA WHERE ARE YOU AND WHAT DO YOU DO AND HOW CAN I HELP YOU NPM RUN DEV NPM HELP ME TO FIND MY SELF WHAT IS LAUGH 
+      //HELLO MINA WHERE ARE YOU AND WHAT DO YOU DO AND HOW CAN I HELP YOU NPM RUN DEV NPM HELP ME TO FIND MY SELF WHAT IS LAUGH
       //
 
       fontFamily: {
