13d3990b39c3a3617f266eebab9aff7d19575b44
MinaNagy99 <minanagykhalefa@gmail.com>
2024-12-19 13:37:40 +0200

    o

--- src/components/CareerPage/PosterCareer/PosterCareer.jsx
+++ src/components/CareerPage/PosterCareer/PosterCareer.jsx
@@ -13,7 +13,7 @@ function PosterCareer() {
         }}
         className="grid grid-cols-7 h-[50vh] object-cover  mt-18 bg-center bg-cover bg-no-repeat"
       >
-        <div className="flex flex-col sm:col-span-6 text-end col-span-6 items-end justify-center lg:ps-10 ps-2   ">
+        <div className="flex flex-col sm:col-span-7  text-end col-span-6 items-end justify-center lg:ps-10 pe-10   ">
           <h1 className="custom-text-xxl font-berlin">
             WOLSEY CAREER <br /> OPPORTUNITIES
             <p className="text-mainBrown custom-text-lg  text-center w-full font-berlin">
@@ -21,16 +21,7 @@ function PosterCareer() {
             </p>
           </h1>
         </div>
-        {/* <img
-          className="sm:col-span-2 col-span-4 w-full  h-[90%] lg:translate-y-[30%]"
-          src="/career/11.webp"
-          alt=""
-        />
-        <img
-          className="sm:col-span-1  col-span-3  h-[90%]  w-full lg:translate-y-[30%]"
-          src="/career/22.webp"
-          alt=""
-        /> */}
+
       </div>
     </>
   );
--- src/components/CertificationPage/certificationsDetails/CertificationDetails.jsx
+++ src/components/CertificationPage/certificationsDetails/CertificationDetails.jsx
@@ -4,12 +4,13 @@ function CertificationDetails() {
   const [widthScreen, setWidthScreen] = useState();
   useEffect(() => {
     setWidthScreen(window.innerWidth);
-[]  }, [widthScreen]);
+    [];
+  }, [widthScreen]);
   return (
     <>
       <div className="bg-mainGray grid md:grid-cols-3 grid-cols-1  xl:gap-20 gap-10 xl:px-20 px-5 py-10">
         <div className="col-span-1 p-8 md:p-0">
-          <img className="w-2/4 m-auto " src="/certifications/c1.png" alt="" />
+          <img className="w-2/4 m-auto " src="/certifications/c2.png" alt="" />
           <p className="custom-text-xxl font-bold py-2 my-4 relative bg-mainBrown  text-white text-center">
             {widthScreen > "1650" && (
               <img
@@ -39,18 +40,14 @@ function CertificationDetails() {
                 Canadian Architect Award of Excellence, 2017. (V20 multi-family
                 project, Calgary)
               </p>
-              {/* <p className="my-2">
-                The 19,450-square-foot BV20 multi-family project in Calgary
-                features sustainable design has won the Canadian Architect Award
-                of Excellence in 2017.
-              </p> */}
+
               <p className="custom-text-xxl font-bold py-2 my-4 bg-mainBrown  text-white text-center">
                 2017
               </p>
 
               <img
                 className="w-2/4 m-auto "
-                src="/certifications/c2.png"
+                src="/certifications/c1.png"
                 alt=""
               />
             </>
@@ -90,8 +87,9 @@ function CertificationDetails() {
             2019
           </p>
           <p className="font-bold my-2 ">
-            Canadian Architect Award of Excellence, 2017 (BV20 multi-family
-            project, Calgary
+            The Alberta Masonry Design Award of Merit for excellence in
+            Landscape urban design and planning, 2019 (Mount Royal Retaining
+            Wall, Calgary)
           </p>
           {/* <p className="my-2">
             The 1880s Mount Royal Retaining Wall restoration, using local
