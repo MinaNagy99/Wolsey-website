9d3ed7307f6d2303b99c70cf22d12e572f2e3b67
MinaNagy99 <minanagykhalefa@gmail.com>
2025-01-10 12:56:13 +0200

    edit text in services page

--- src/components/Services/servicesBrief/servicesBrief.jsx
+++ src/components/Services/servicesBrief/servicesBrief.jsx
@@ -24,26 +24,27 @@ function ServicesBrief() {
           </h2>
           <div className="md:w-1/2 text-justify">
             <p className={` custom-text-base mb-3 `}>
-              With a legacy of over 1,000 projects spanning four continents,
-              Wolsey's portfolio exemplifies a steadfast commitment to innovation,
-              precision, and excellence. Our work transcends boundaries,
-              encompassing a diverse range of sectors, from visionary master
-              planning and transformative urban designs to iconic cultural
-              landmarks, advanced healthcare facilities, and modern residential
-              and commercial spaces.
+              With a legacy of over 1,000 projects across four continents,
+              Wolsey's portfolio reflects an unwavering dedication to
+              innovation, precision, and engineering excellence. Our expertise
+              spans diverse sectors, including large-scale infrastructure,
+              iconic cultural landmarks, advanced healthcare facilities,
+              industrial complexes, and modern residential and commercial
+              developments.
             </p>
             <p className={` custom-text-base mb-3 `}>
-              Every project integrates functionality, aesthetic sophistication,
-              and sustainable design principles, ensuring lasting impact and
-              relevance in a rapidly evolving global landscape. At Wolsey, we are
-              dedicated to shaping communities and redefining spaces with
-              designs that inspire, connect, and endure.
+              At Wolsey, we seamlessly integrate structural integrity, technical
+              ingenuity, and sustainable engineering practices to deliver
+              solutions that stand the test of time. Every project is designed
+              to meet functional demands while optimizing performance, safety,
+              and efficiency, ensuring a lasting impact on the built
+              environment.
             </p>
             <p className={` custom-text-base mb-3 `}>
-              This track record of success reflects not only our technical
-              expertise but also our collaborative ethos and unwavering
-              dedication to enhancing the built environment through thoughtful
-              and innovative architectural solutions.
+              This proven track record underscores not only our technical
+              expertise but also our collaborative approach and commitment to
+              enhancing communities through thoughtful, cutting-edge structural
+              engineering solutions.
             </p>
           </div>
         </div>
